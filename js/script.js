d93183353aeaf9197dce5e77592118dca144a96b
Elvira Reshetnikova <83705191+ElviraResh@users.noreply.github.com>
2021-07-12 21:22:14 +0500

    Task-7
    Добавлена интерполяция

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@
 /* const string = 'hello';
 
 const string1 = "they call \"Harry\", and he likes it"; // с помощью \ можно экранировать значение символа */
-// конкатинация - склеивание строки
+/* конкатинация - склеивание строки (таким способом лучше не пользоваться, так как может подтянуть за собой очень много ошибок, т.е. результат может получиться не тот, который ожидаете) */
 
 
-const skip = str => {
+/* const skip = str => {
     let i = 0;
     let result = '';
     while (i < str.length) {
@@ -16,7 +16,7 @@ const skip = str => {
     }
     return result;
 }
-console.log(skip('California'));
+console.log(skip('California')); */
 
 // length - длина
 // квадртные скобки позволяют получать индивидуальные символы из строки
@@ -24,3 +24,25 @@ console.log(skip('California'));
 // 'California'
 // количество символов в строке 10
 // количество индексов 9 (индексация элементов в строке начинается с 0)
+
+//Неизменяемость (immutable)
+// в js строки неизменяемы. какие бы функции не применяли, они не производят изменения первоначальной строки
+
+/* const str = '    hello    ';
+console.log(str.toUpperCase()); // HELLO
+console.log(str); // hello
+console.log(str[0].toUpperCase()); // H
+console.log(str.trim()); // метод. который убирает лишние пробелы */
+
+// лексикографический порядок
+
+/* 'ab' > 'bc' // false
+'8' > '7' // true */
+
+// интерполяция 
+// кроме '' и "" в js есть `` (backticks);
+
+const name = 'Elvira';
+const a = 10;
+const b = 20;
+console.log(`Her name is ${name} and her age is ${a + b}`); // такие синтаксические конструкции работают только с обратными кавычками ``;
