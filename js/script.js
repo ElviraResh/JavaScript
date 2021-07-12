18d92072373dbab6de135c02db9b4c79126ca44d
Elvira Reshetnikova <83705191+ElviraResh@users.noreply.github.com>
2021-07-12 23:19:05 +0500

    Task-6
    Инкременты, декременты, постфиксные и префиксные нотации (формы), цикл for

--- js/script.js
+++ js/script.js
@@ -13,14 +13,48 @@
     let result = 1; // текущий результат
 
     while (counter <= n) {
-        result = result * counter;
-        counter = counter + 1;
+        result = result * counter; // равносилен result *=counter; 
+        counter = counter + 1; // равносилен counter++ (инкремент), counter-- (декремент)
         console.log(result);
     }
     return result;
 }
 
 console.log(factorial(3)); */
+
+// инкремент, декремент
+// postfix
+
+/* let a = 3;
+let b;
+b = a++; // постфиксная нотация (фактическое сложение произойдет после того, как значение вернется)
+console.log(a, b); // a = 4; b = 3;
+
+// prefix
+
+let a = 3;
+let b;
+b = ++a; // префиксная нотация (фактическое сложение произойдет перед тем, как значение вернется)
+console.log(a, b); // a = 4; b = 43; */
+
+// Пример
+
+/* let x = 45;
+let height = x++;
+x = --height; */
+
+/* let a = 4;
+a -= 8 - a;
+
+console.log(a); // 0 */
+
+/* let b = 10;
+b += b ** 2 > 20 ? 1 : 10; // ? - условие, если b ** 2 больше 20, то вернуть значение 1
+console.log(b); // 11  */
+
+/* let x = 5;
+let y = 10;
+console.log(x++ + ++y); */
 
 //Задача.
 // Реализовать тело функции, используя итеративный процесс.
@@ -75,3 +109,103 @@ console.log(smallestDivisor(1)); */
 };
 
 while (true) {console.log("...")} */
+
+// цикл for (более универсальный цикл)
+
+/* const factorial = n => {
+    let result = 1; // переменная счетчика, чтобы считать от 1 до верхнего предела
+    for (let counter = 1; counter <= n; counter++) {
+        result *= counter;
+    }
+    return result;
+}
+console.log(factorial(3)); */
+
+// инициализация счетчика - let counter = 1;
+// условие цикла - counter <= n;
+// обновление счетчика - counter++;
+// тело цикла - {result *= counter;}
+
+// в блоке инициализации не требуется определять переменные
+/* let counter = 1;
+for (; counter <= n); counter++) {
+    // любой код
+} */
+
+// блок с условием не обязателен
+/* for (let counter = 1;; counter++) {
+    if (counter <= n) break;
+    // любой код
+} */
+
+// пропущены все 3 блока
+/* let counter = 1;
+for (;;) {
+    if (counter <= n) break;
+    // любой код
+    counter++;
+} */
+
+/* let answer;
+
+if (num === 1) {
+    answer = "One";
+} else if (num === 2) {
+    answer = "Two";
+} else {
+    answer = "Nothing";
+} */
+
+// конструкция SWITCHCASE
+/* switch(num) {
+    case 1: // if (num === 1);
+        answer = "One";
+        break;
+    case 2: // if (num === 2);
+        answer = "Two";
+        break;
+    default: 
+        answer = "Nothing";
+        break;  
+} */
+
+// Задача
+/* const absoluteNumber = (num) => {
+    if (num > 0 || num === 0) {
+        return num;
+    } else {
+    return -num;
+    }
+} */
+
+/* switch(num) {
+    case 1: num > 0;
+        num;
+        break;
+    default: 
+        -num;
+        break;
+}
+
+console.log(-5); */
+
+// функция должна считать сумму всех чисел от 1 до n
+/* const sum = n => {
+    let result = 0;
+    for (let counter = 1; counter <= n; counter++) {
+        return 10; // return прерывает цикл
+        result += counter;
+    }
+    return result;
+} 
+
+console.log(sum(5)); // 10
+console.log(sum(20)); // 10
+console.log(sum(50)); // 10 */
+
+// Домашнее задание
+// Написать функцию, которая принимает число и возвращает true если число является простым и false в ином случае
+
+/* простое число - целое положительное число, имеющее ровно два различных натуральных делителя - единицу и самого себя */
+
+
