2e4df215b5749ac46ac257718413626d800cb06d
Elvira Reshetnikova <83705191+ElviraResh@users.noreply.github.com>
2021-08-16 21:51:55 +0500

    Task-12
    Добавление методов

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict'
 
-//DOM - document object model
+//DOM - document object model - это представление документа в виде дерева тегов. Это дерево образуется за счет вложенной структуры тегов плюс текстовые фрагменты страницы, каждый из которых образует отдельный узел.
+// 
 
 //window - глобальный объект, предоставляемый браузером, внутри которого содержатся все встроенные в браузерный JS функции и св-ва 
 /* alert('hello!'); // window.alert('hello!')
@@ -13,7 +14,7 @@ Math.abs(5); // window.Math.abs(5) */
 // history - можно перемещаться по истории переходов, а так же формировать ее в тех ситуациях, когда не происходит реального перехода по страницам
 // fetch - современный метод выполнения ajax запросов. именно с помощью функции fetch происходит общение с сервером и другими сайтами (fetch('https://www.airbnb.ru').then((res)=>res.text()).then(console.log(body));)
 
-//DNS - domen name system 
+//DNS - domen name system - доменная система имен
 
 // что делает браузер для отображения страницы:
 // 1) браузер делает запрос на сервер (предварительно выяснив этот адрес с помощью DNS)
@@ -43,3 +44,14 @@ console.log(ul);
 
 const spans = ul.querySelectorAll('.odd > span');
 console.log(spans); */
+
+//метод matches - проверяет, удовлетворят ли el селектору css
+const el = document.querySelector('p');
+console.log(el.matches('.unknow-class'));
+console.log(el.matches('.font-weitgh'));
+
+// метод closest - ищет ближайший элемент выше по ирархии, удовлетворяющий селектору. Сам элемент тоже анализируется. Если такой элемент найден, то возвращается он, иначе возвращается null
+
+const element = document.querySelector('span');
+const ancestor = element.closest('.row');
+console.log(ancestor.id); // three
