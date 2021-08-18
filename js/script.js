f580773acf307fa65bbd8a04765627a421b1a875
ElviraReshetnikova <nikkason@mail.ru>
2021-08-18 12:23:01 +0500

    Task-12
    Методы matches, closest. Манипулирование DOM-деревом, управление узлами

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 
 //window - глобальный объект, предоставляемый браузером, внутри которого содержатся все встроенные в браузерный JS функции и св-ва 
 /* alert('hello!'); // window.alert('hello!')
-Math.abs(5); // window.Math.abs(5) */ 
+Math.abs(5); // window.Math.abs(5) */
 
 //BOM - browser object model - набор глобальных объектов, управляющих поведением браузера. все они находятся внутри window
 // 1. navigator - информация о браузере (версия, название, доступные права, подключенные плагины)
@@ -46,12 +46,68 @@ const spans = ul.querySelectorAll('.odd > span');
 console.log(spans); */
 
 //метод matches - проверяет, удовлетворят ли el селектору css
-const el = document.querySelector('p');
-console.log(el.matches('.unknow-class'));
-console.log(el.matches('.font-weitgh'));
+/* const el = document.querySelector('p');
+console.log(el.matches('.unknow-class')); // false
+console.log(el.matches('.font-weitgh')); // true */
 
 // метод closest - ищет ближайший элемент выше по ирархии, удовлетворяющий селектору. Сам элемент тоже анализируется. Если такой элемент найден, то возвращается он, иначе возвращается null
 
-const element = document.querySelector('span');
-const ancestor = element.closest('.row');
-console.log(ancestor.id); // three
+// const element = document.querySelector('span');
+// const ancestor = element.closest('.row');
+// console.log(ancestor.id); // three
+
+// манипулирование DOM-деревом
+// innerHTML - свойство, которое целиком заменяет потомков элемента
+
+// const body = document.body;
+// console.log(body);
+
+// body.innerHTML = '<b>make</b> love';
+// console.log(body.innerHTML);
+
+//textContent - то же самое, но только рассматривает содержимое как обычный текст
+// const body = document.body;
+// document.body.textContent = '<b>make</b> love';
+// console.log(document.body.innerHTML);
+
+// создание узлов
+
+// создаем текстовый узел
+// const textNode = document.createTextNode('life is life');
+// создаем элемент p
+// const pElement = document.createElement('p');
+// console.log(textNode);
+// console.log(pElement);
+
+// добавляем textNode в конец списка элемента pEl
+// console.log(pElement.append(textNode));
+// console.log(pElement.textContent = 'Life is life');
+
+// создаем div
+// const element = document.createElement('div');
+// element.append(pElement);
+// console.log(element);
+
+// управление узлами DOM
+// атрибуты
+// когда браузер загрузил HTML, на его основе строится DOM.
+// во время обработки каждый тег становится узлом, а атрибуты - свойствами этого узла. 
+
+// const element = document.querySelector('#aboutPage');
+// console.log(element);
+
+// console.log(element.id); // aboutPage
+// console.log(element.href); // /pages/about
+
+// добавление/удаление класса
+// const element = document.querySelector('#aboutPage');
+// element.classList.add('page'); // добавление класса
+// console.log(element);
+
+// element.classList.remove('simple-link');
+// console.log(element); // удаление класса
+
+// element.classList.contains('class') - проверяет содержит ли элемент нужный класс
+
+// element.classList.toggle('class') - если есть класс, удаляет его и наоборот
+
